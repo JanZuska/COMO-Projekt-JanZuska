ea569569ad8edaf352ea684e728a1d3ef05aba60
Jan Zuska <jan.zuska.04@gmail.com>
2023-04-09 16:43:48 +0200

    Applied SearchQuerryValuesRequest to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from lokality import Lokality
+from search import SearchQueryValuesRequest
 
 # Kontrola, zda existuje soubor kraje.txt
 try:
@@ -22,6 +23,16 @@ except:
     with open("okresy.txt", "rb") as file:
         okresy = file.read().decode("windows-1250")[1:-1].replace("'", "").split(", ")
 
+# Session states
+session_states_list = [
+    {"kraj" : "Hlavní město Praha"}
+]
+
+for item in session_states_list:
+    for key, value in item.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
+
 okresy = {}
 for okres in okresy_1:
     okres = okres.split(" : ")
@@ -31,13 +42,24 @@ seznam_okresu = []
 for key, value in sorted(okresy.items()):
     if value == st.session_state.kraj:
         seznam_okresu.append(key)
+        st.session_state.okres_selectbox_active = False
 
-if "zvoleny_okres" not in st.session_state:
-    st.session_state["zvoleny_okres"] = 0
-
+if (st.session_state.kraj == "Hlavní město Praha") or (st.session_state.kraj == None):
+    st.session_state.okres_selectbox_active = True
+    seznam_okresu = ["Žádné okresy"]
 
 with st.sidebar:
     st.write(st.session_state.kraj)
-    st.selectbox(label="CHCI", options=["Prodej", "Pronájem"], key="chci")
-    st.selectbox(label="KRAJ", options=kraje, key="kraj")
-    x = st.selectbox(label="OKRES", options=seznam_okresu, key="okres")
+    chci_selectbox = st.selectbox(label="CHCI", options=["Prodej", "Pronájem"], key="chci")
+    kraj_selectbox = st.selectbox(label="KRAJ", options=kraje, key="kraj")
+    okres_multiselect = st.multiselect(label="OKRES", options=seznam_okresu, key="okres", disabled=st.session_state.okres_selectbox_active)
+    vyhledat_button = st.button(label="VYHLEDAT", key="vyhledat")
+    #cena_slider = st.slider(label="CENA", min_value=0, max_value=100, value=(0,100), key="cena")
+
+if vyhledat_button:
+    if st.session_state.okres == []:
+        st.write("Nic")
+    else:
+        for okres in st.session_state.okres:
+            query_values = SearchQueryValuesRequest(okres).GetQueryValues()
+            search_query = f"https://www.bezrealitky.cz/vyhledat?offerType=PRONAJEM&estateType=BYT&regionOsmIds=R{query_values['osm_id']}&osm_value={query_values['display_name']}"
